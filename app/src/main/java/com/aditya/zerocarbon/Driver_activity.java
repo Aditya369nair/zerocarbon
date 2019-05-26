0913a4efb2f350c9ca24409a9848a72740246b91
Aditya Nair <aditya.nair@student.isgr.se>
2019-05-26 15:13:16 +0200

    clean up

--- app/src/main/java/com/aditya/zerocarbon/Driver_activity.java
+++ app/src/main/java/com/aditya/zerocarbon/Driver_activity.java
@@ -1,37 +1,11 @@
 package com.aditya.zerocarbon;
 
-import android.location.Address;
-import android.location.Geocoder;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.GeoPoint;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class Driver_activity extends AppCompatActivity {
-
-    public static final String FROM_FIELD = "from";
-    public static final String TO_FIELD = "to";
-    public static final String SEAT_FIELD = "seats";
-    public static final String DATE_FIELD = "date";
-
-    private CollectionReference rides = FirebaseFirestore.getInstance().collection("rides");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
